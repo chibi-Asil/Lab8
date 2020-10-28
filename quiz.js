8c816c1801793ed818699c7dd901f3e89bd10de1
chibi-Asil <32022127+chibi-Asil@users.noreply.github.com>
2020-10-27 20:22:25 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -6,7 +6,6 @@
 var pos = 0 // Is used to record what the position the user is at in the test eg. question 3 out of 4.
 var correct = 0 // Shows how many you answered correctly
 var mc_test, mc_test_status, mc_question, button, buttons, chA, chB, chC, chD; // The test variable contains the ‘test’ div and the test_status variable is used to contain the ‘test_status’ heading which displays the user’s progress in the test. The question variable will contain a question grabbed from an array, the choice variable will contain the user’s selected answer for a question, choices variable will contain the possible answers that will be displayed to the user, and chA, chB, and chC variables will contain each possible answer (A, B, or C) in the multiple-choice quiz. Since my question has 4 choices, it is choice_a, choice_b, choice_c, choice_d. We also have a fill_in_blank question
-var blank_test, blank_test_status, blank_question, fill_in_blank // This is for fill-in-the blank question
 
 //Then we will need to create the questions 
 // this is a multidimensional array with 5 inner array elements with 6 elements inside them
@@ -45,23 +44,6 @@ var mc_questions = [
     }
     ];
 
-var blank_questions = [
-    {
-        blank_question: "Monsieur Hood sings, 'I like an honest fight and a saucy little maid.' Merry Men sings, 'What he's basically saying is he likes to get...'",
-        blank_box:" ", 
-        answer: "laid",
-    },
-    {
-        blank_question: "What’s the difference between a Catholic priest and a zit?",
-        blank_box: " ",
-        answer: "A zit will wait until you’re twelve before it comes on your face.",
-    },
-    {
-        blank_question: "Although she lives with seven other men, she is not easy. Who is she?"
-        blank_box: " ",
-        answer: "Snow White",
-    }
-    ];
 // Next step would be to create the get() function
 function get(x){
     return document.getElementById(x);
@@ -97,23 +79,24 @@ function mcQuestion(){
     mc_test.innerHTML +="<label><input type = 'button' name = 'button' value = 'D'>" + chaD + "</label><br>";
 }
 // Creating a function to check for the answers
-
-// Part 2 questions
-function blankQuestion(){
-        blank_test = get("blank_test");
-    if(pos >= blank_questions.length){
-        blank_test.innerHTML = "<h3> You got " + correct + " of " + blank_questions.length + "questions correct.</h3>";
-        get("blank_test_status").innerHTML = "Part 2 is now completed. You earned your freedom...or did you really? Because you know what you said...and we captured your data.";
-        pos++;
-        correct++;
-        return false;
+function mcAnswer(){
+    // use the getElementByName because we have an array which it can loop through
+    buttons = document.getElementsByName("buttons");
+    for (var i = 0; i < buttons.length; i++){
+        if(buttons[i].checked){
+            button = buttons[i].value;    
+        }
     }
-get("blank_test_status").innerHTML = "Question "+ (pos+1) + " of "+ blank_questions.length;
-blank_question = blank_questions[pos].question;
-chA = blank_questions[pos].a;
-chB = blank_questions[pos].b;
-chC = blank_questions[pos].c;
-chD = blank_questions[pos].d;
+    // Checks if answer matches the correct choice
+    if(choice == questions[pos].answer){
+        correct++;
+    }
+    // Which then changes the position on which the user is now on 
+    pos++;
+    mcQuestion();
+}
+// Adding an event listner to call mcAnswer on the page load event
+window.addEventListner("load", mcAnswer);
 
 
 
